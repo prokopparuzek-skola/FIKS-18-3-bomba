c873a466652a4e6a1dda0d27f1da448f745f6dc4
prokopparuzek <prokop.paruzek@paruzkovi.cz>
2019-04-08 20:11:25 +0200

    0.1

--- bomba.c
+++ bomba.c
@@ -3,7 +3,7 @@
 int main() {
     int n, m;
     int draty[4];
-    int *last, *ac; 
+    int *last, *ac, *swap;
     last = &draty[2];
     last[0] = -1;
     last[1] = -1;
@@ -14,13 +14,15 @@ int main() {
         scanf("%d%d", &ac[0], &ac[1]);
         if (*last != -1) {
             if ((ac[0] == last[1] && ac[1] > last[1]) || (ac[0] == last[0] && ac[1] == last[1])) {
-                continue;
             }
             else {
                 puts("tddm");
                 return 0;
             }
         }
+        swap = ac;
+        ac = last;
+        last = swap;
     }
     puts("bum");
     return 0;
